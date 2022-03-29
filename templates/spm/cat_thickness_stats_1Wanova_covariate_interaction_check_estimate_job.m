2ce97235764e12e2830ba970f6e18ea6c7549378
alberto.inuggi@iit.it <qwas_QWAS09>
2022-03-29 17:24:39 +0200

    stats code and spm templates refactoring

--- templates/spm/cat_thickness_stats_1Wanova_covariate_interaction_check_estimate_job.m
+++ templates/spm/cat_thickness_stats_1Wanova_covariate_interaction_check_estimate_job.m
@@ -1,14 +1,17 @@
 %-----------------------------------------------------------------------
-% Job saved on 20-Aug-2019 13:46:21 by cfg_util (rev $Rev: 7345 $)
+% Job saved on 20-Sep-2019 15:54:35 by cfg_util (rev $Rev: 7345 $)
 % spm SPM - SPM12 (7487)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.stats.factorial_design.dir = {'<STATS_DIR>'};
-<GROUP_IMAGES>
-matlabbatch{1}.spm.stats.factorial_design.des.anova.dept = 0;
-matlabbatch{1}.spm.stats.factorial_design.des.anova.variance = 1;
-matlabbatch{1}.spm.stats.factorial_design.des.anova.gmsca = 0;
-matlabbatch{1}.spm.stats.factorial_design.des.anova.ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.name = '<FACTOR1_NAME>';
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.levels = <FACTOR1_NLEV>;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.ancova = 0;
+<FACTORS_CELLS>
+matlabbatch{1}.spm.stats.factorial_design.des.fd.contrasts = 1;
 <COV_STRING>
 matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
@@ -21,3 +24,6 @@ matlabbatch{2}.spm.tools.cat.tools.check_SPM.spmmat(1) = cfg_dep('Factorial desi
 matlabbatch{2}.spm.tools.cat.tools.check_SPM.check_SPM_cov.do_check_cov.use_unsmoothed_data = 1;
 matlabbatch{2}.spm.tools.cat.tools.check_SPM.check_SPM_cov.do_check_cov.adjust_data = 1;
 matlabbatch{2}.spm.tools.cat.tools.check_SPM.check_SPM_ortho = 1;
+matlabbatch{3}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{3}.spm.stats.fmri_est.method.Classical = 1;
